a2604c886358e8f44bcd374d1e4b4437dec19ca8
emad290 <emadrasmy26@gmail.com>
2025-11-02 14:38:56 +0200

    Fix middleware authentication using NextAuth JWT

--- src/middleware.js
+++ src/middleware.js
@@ -13,24 +13,48 @@
 
 
 
-import { NextResponse } from 'next/server'
+// import { NextResponse } from 'next/server'
 
-export function middleware(request) {
-  const token = request.cookies.get("token")?.value;
+// export function middleware(request) {
+//   const token = request.cookies.get("token")?.value;
 
-  // الصفحات اللي محتاجه حماية
-  const protectedRoutes = ["/", "/categories", "/products","/cartgit add ."];
+//   // الصفحات اللي محتاجه حماية
+//   const protectedRoutes = ["/", "/categories", "/products","/cart"];
 
-  if (protectedRoutes.includes(new URL(request.url).pathname)) {
+//   if (protectedRoutes.includes(new URL(request.url).pathname)) {
+//     if (!token) {
+//       return NextResponse.redirect(new URL("/login", request.url));
+//     }
+//   }
+
+//   return NextResponse.next();
+// }
+
+// // التحديد للمسارات اللي بيتطبق عليها الميدل وير
+// export const config = {
+//   matcher: ["/", "/categories", "/products","/cart"],
+// };
+
+
+
+
+import { getToken } from "next-auth/jwt"
+import { NextResponse } from "next/server"
+
+export async function middleware(req) {
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET })
+
+  const protectedRoutes = ["/", "/categories", "/products", "/cart"]
+
+  if (protectedRoutes.includes(new URL(req.url).pathname)) {
     if (!token) {
-      return NextResponse.redirect(new URL("/login", request.url));
+      return NextResponse.redirect(new URL("/login", req.url))
     }
   }
 
-  return NextResponse.next();
+  return NextResponse.next()
 }
 
-// التحديد للمسارات اللي بيتطبق عليها الميدل وير
 export const config = {
-  matcher: ["/", "/categories", "/products","/cart"],
-};
+  matcher: ["/", "/categories", "/products", "/cart"],
+}
