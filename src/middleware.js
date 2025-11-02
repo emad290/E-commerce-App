e7c05598a1b76c8b00219333d65056548e773bc3
emad290 <emadrasmy26@gmail.com>
2025-11-02 13:52:27 +0200

    Fix middleware export for Vercel

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,36 @@
-export { default } from "next-auth/middleware";
+// export { default } from "next-auth/middleware";
 
+// export const config = {
+//   matcher: [
+//     "/",               // الصفحة الرئيسية
+//     "/categories/:path*", 
+//     "/products/:path*",
+//     "/cart/:path*"
+//   ],
+// };
+
+
+
+
+
+import { NextResponse } from 'next/server'
+
+export function middleware(request) {
+  const token = request.cookies.get("token")?.value;
+
+  // الصفحات اللي محتاجه حماية
+  const protectedRoutes = ["/", "/categories", "/products","/cartgit add ."];
+
+  if (protectedRoutes.includes(new URL(request.url).pathname)) {
+    if (!token) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+// التحديد للمسارات اللي بيتطبق عليها الميدل وير
 export const config = {
-  matcher: [
-    "/",               // الصفحة الرئيسية
-    "/categories/:path*", 
-    "/products/:path*",
-    "/cart/:path*"
-  ],
+  matcher: ["/", "/categories", "/products","/cart"],
 };
